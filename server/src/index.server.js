1538c74d4a8e08c64c86d7cb1007dd583a680cf7
haseebasifdev <haseebasif147@gmail.com>
2021-01-08 17:29:47 +0500

    ADDING .ENV FILES

--- server/src/index.server.js
+++ server/src/index.server.js
@@ -6,14 +6,13 @@ const cors = require('cors');
 app.use(cors());
 const mongoose = require("mongoose");
 app.use(express.json());
-const mongoURL="mongodb+srv://haseebasif:cWrVW6IHlpXBbx0m@cluster0.mwvu1.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const contactRoute=require("./route/ContactMe")
 app.get("/", (req, res) => {
     res.send("Hello to the future")
 });
 const port = process.env.PORT || 5000;
 app.use("/api",contactRoute)
-mongoose.connect(mongoURL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODBURL,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(res=>{
     console.log("Database Connected");
     app.listen(port, () =>console.log( `Server running on port ${port} 🔥`));
@@ -23,4 +22,3 @@ mongoose.connect(mongoURL,{useNewUrlParser: true, useUnifiedTopology: true})
 })
 
 
-// cWrVW6IHlpXBbx0m
